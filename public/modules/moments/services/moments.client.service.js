bd5c655d2eb26683761bd708172be469e4c6bc9a
Robbert van Markus <rvanmarkus@xebia.com>
2015-09-12 04:56:36 +0200

    moments module

---
+++ public/modules/moments/services/moments.client.service.js
@@ -0,0 +1,12 @@
+'use strict';
+
+//Moments service used to communicate Moments REST endpoints
+angular.module('moments').factory('Moments', ['$resource', function($resource) {
+    return $resource('moments/:momentId', {
+        momentId: '@_id'
+    }, {
+        update: {
+            method: 'PUT'
+        }
+    });
+}]);
