2801a0856230c35b92c4569d669367a88565a78f
SirLucian <lucian.blankevoort@gmail.com>
2015-09-27 01:34:26 +0200

    virtual repeating list

--- src/app/index.run.ts
+++ src/app/index.run.ts
@@ -1,11 +1,11 @@
 /** @ngInject */
 export function runBlock($rootScope, $state, $log: ng.ILogService, userServices) {
   $log.debug('runBlock end');
-  // Listen to '$locationChangeSuccess', not '$stateChangeStart'
-  // $rootScope.$on('$locationChangeSuccess', function() {
-  //   console.log(userServices.userIsLoggedIn);
-  //   if(! userServices.userIsLoggedIn() && $state.name != 'login'){
-  //     $state.go('login')
-  //   }
-  // })
+  //Listen to '$locationChangeSuccess', not '$stateChangeStart'
+  $rootScope.$on('$locationChangeSuccess', function() {
+    console.log(userServices.userIsLoggedIn);
+    if(! userServices.userIsLoggedIn() && $state.name != 'login'){
+      $state.go('login')
+    }
+  })
 }
