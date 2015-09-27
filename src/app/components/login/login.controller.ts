7b7096f5fc93f35baee62039180f2c9b5f15f636
Robbert van Markus <rvanmarkus@xebia.com>
2015-09-27 06:34:18 +0200

    login indicator, fixes

--- src/app/components/login/login.controller.ts
+++ src/app/components/login/login.controller.ts
@@ -4,19 +4,24 @@ export class LoginController {
   private userServices;
   private $mdToast;
   private location;
+  public loadingIndicator;
+
   constructor($scope, userServices, $location, $mdToast: any){
     this.location = $location;
     this.userServices = userServices;
     this.$mdToast = $mdToast;
+    this.loadingIndicator = false;
   }
 
   loginSuccessfull() {
     this.$mdToast.show(
       this.$mdToast.simple().content('Login Successfull!').hideDelay(2500));
+      this.loadingIndicator = false;
       this.location.path('/');
   }
 
   authenticate() {
+    this.loadingIndicator = true;
     this.userServices.authenticate().then((user)=>{
       this.loginSuccessfull();
       this.user = user;
